f5a51468ec92aa7cb5e473f71da3cff8a33a354b
Michael Woods <mrwoodzi@protonmail.com>
2021-02-04 15:24:35 -0600

    free_code_camp

--- percentage_calc.py
+++ percentage_calc.py
@@ -1,11 +1,13 @@
+
+# I wrote this by myself not because it was apart of an example but since I needed to figure some percentages 
 percentage = input("Input Percentage in decimals: ")
 number     = input("Input Number: ")
 
 p = float(percentage)
 n = float(number)
 
-percent = (p * n) - n 
-positive = percent * -1.0
-print(f"Your percentage of the input number is {positive}")
+percent  = ((p * n) - n) * -1.0 
+
+print(f"Your percentage of the input number is {percent}")
 
 
